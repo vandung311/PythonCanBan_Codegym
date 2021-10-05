98074b93e07135103e146bb8ac0f58b701dc8ec0
TriDung <vandung311@gmail.com>
2021-10-05 21:45:30 +0700

    Nop bai tap

---
+++ Bai8_02.10_KiemTraSoChanLe_VanDung.py
@@ -0,0 +1,8 @@
+# Kiểm tra số chẵn lẻ
+Number = float(input("Nhập giá trị:"))
+if(Number%2==0):
+    print("Giá trị nhập vào là số chẵn")
+elif(Number%2==1):
+    print("Giá trị nhập vào là số lẻ")
+else:
+    print("Không phải là số tự nhiên")
